0dc8ed7d18cf3fed769e673a9d458e9c4a4817ae
Baraa <156128855+Baraa-op@users.noreply.github.com>
2026-01-28 20:32:18 +0300

    Add initial C++ program to display course marks

---
+++ 2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdio>
+#include "input.h"
+#include <iomanip> 
+using namespace std;
+
+
+
+int main()
+{
+	cout << "--------|-------------------------------------|------------------\n";
+	cout << "  CODE  |                 NAME                |       MARK       \n" ;
+	cout << "________|_____________________________________|__________________\n";
+	
+	cout << setw(8) <<  "CS50e" <<"|" << setw(37) << "introduction to programing" <<"|" << setw(19) << "94\n" ;
+	cout << setw(8) <<  "adv101pr" <<"|" << setw(37) << "c++ advanced programing" <<"|" << setw(19) << "80\n" ;
+    cout << setw(8) <<  "CS50e" <<"|" << setw(37) << "pointers vectors and reference" <<"|" << setw(19) << "78\n" ;
+    cout << "________|_____________________________________|__________________\n";
+
+    
+    
+}
